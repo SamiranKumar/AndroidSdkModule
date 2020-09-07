c33217298eee760d29872811fe8c6f52d748c627
Samiran Kumar <samirankumar11@gmail.com>
2020-09-07 17:27:33 +0600

    activity ui change

--- Androidsdk/app/src/main/java/com/live/tech/cinematic/sdk/android_sdk/MainActivity.java
+++ Androidsdk/app/src/main/java/com/live/tech/cinematic/sdk/android_sdk/MainActivity.java
@@ -2,9 +2,11 @@ package com.live.tech.cinematic.sdk.android_sdk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.appsghor.common.AppLog;
+import com.appsghor.common.MyActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppLog.print("Test Print");
+        startActivity(new Intent(this, MyActivity.class));
 
     }
 }
