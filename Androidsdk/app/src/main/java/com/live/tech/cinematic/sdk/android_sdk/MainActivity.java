2dad3130d79434a516e28cbc20b856e7a143b053
Samiran Kumar <samirankumar11@gmail.com>
2020-09-07 16:43:07 +0600

    Create project

---
+++ Androidsdk/app/src/main/java/com/live/tech/cinematic/sdk/android_sdk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.live.tech.cinematic.sdk.android_sdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
